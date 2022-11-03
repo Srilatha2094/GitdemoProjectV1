f31b7d9c438f12ce2e3f8f78005c4fe2388cda88
Stat <Stat@DESKTOP-34SL403>
2022-11-03 13:39:34 -0700

    First Commit

---
+++ src/ArrayExample.java
@@ -0,0 +1,26 @@
+
+
+public class ArrayExample {
+
+	
+	public static void main(String agrs[]) {
+		
+		
+		int[] arr = new int[5];
+		
+		
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+		
+		
+		for(int i=0; i< arr.length; i++) {
+			
+			System.out.println("Array of Elements:" + arr[i]);
+		}
+		
+		
+	}
+}
